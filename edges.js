0a00af0825544861c633cc90349c4d9b250ca78b
windowsloth <67842472+windowsloth@users.noreply.github.com>
2020-08-12 12:06:06 -0400

    Update edges.js

--- edges.js
+++ edges.js
@@ -3,36 +3,13 @@ class Edges {
     // this.e = e;
     this.S = [];
   }
-  add(org, dest) {
-    const e = new MakeEdge();
-    e.DATA = this.S[org].SYM.DATA;
-    //e.DATA = this.S[this.S.length - 1].SYM.DATA;
-    e.sym(dest);
-    //e.cleave(this.S[this.S.length -1]);
-    e.cleave(this.S[org].SYM);
 
-    if (this.S[org].LNEXT = this.S[org].SYM) {
-      this.S[org].LNEXT = e;
-    }
-    e.cleave(this.S[org].LNEXT);
-
-    this.S.splice(this.S.length, 0, e);
-  }
   connect(a, b) {
     const e = new MakeEdge();
     e.setup(a.SYM().DATA, b.DATA);
     e.cleave(a.LNEXT());
     e.SYM().cleave(b);
     this.S.splice(this.S.length, 0, e);
-
-  //   const e = new MakeEdge();
-  //   e.DATA = a.SYM.DATA;
-  //   e.sym(b.DATA);
-  //   //e.DATA = a.SYM.DATA;
-  // //  e.SYM.DATA = b.DATA;
-  //   e.cleave(a.LNEXT);
-  //   e.SYM.cleave(b);
-  //   this.S.splice(this.S.length, 0, e);
   }
   destroy(e) {
     e.cleave(e.OPREV());
@@ -42,27 +19,17 @@ class Edges {
         this.S.splice(this.S.indexOf(id, 0), 1);
       }
     }
-
-    // //there's almost no way this works
-    // let id = e.DATA;
-    // e.cleave(e.SYM.LNEXT);
-    // e.SYM.cleave(e.SYM.SYM.LNEXT);
-    // for (e of this.S) {
-    //   if (e.DATA = id) {
-    //     this.S.splice(this.S.indexOf(id, 0), 1);
-    //   }
-    // }
   }
   swap(e) {
-    //same for this
-    let a = e.SYM.LNEXT;
-    let b = e.SYM.SYM.LNEXT;
+    //May or may not work?
+    let a = e.OPREV();
+    let b = e.SYM.OPREV();
     e.cleave(a);
-    e.SYM.cleave(b);
-    e.cleave(a.LNEXT);
-    e.SYM.cleave(b.LNEXT);
-    e.DATA = a.SYM.DATA;
-    e.SYM.DATA = b.DATA;
+    e.SYM().cleave(b);
+    e.cleave(a.LNEXT());
+    e.SYM().cleave(b.LNEXT());
+    e.DATA = a.SYM().DATA;
+    e.SYM().DATA = b.DATA;
   }
   show() {
     for (let edge of this.S) {
@@ -75,17 +42,9 @@ class Edges {
 
 class MakeEdge {
   constructor() {
-    /*this.DATA = null;
-    this.SYM = null;
-    this.ONEXT = this;
-    //this.OPREV = this;
-    this.LNEXT = null;
-    //this.RNEXT = this.SYM;*/
-
     this.DATA = null;
     this.NEXT = null;
     this.r = [this, null, null, null];
-    //this.f = [];
   }
   setup(org, dest) {
     this.DATA = org;
@@ -140,7 +99,6 @@ class MakeEdge {
     return this.r[(n + 1) % 4].NEXT.ROT();
   }
   OPREV() {
-    //return this.r[1].NEXT.ROT();
     return this.ROT().NEXT.ROT();
   }
   LNEXT() {
@@ -149,17 +107,7 @@ class MakeEdge {
   RPREV() {
     return this.SYM().ONEXT();
   }
-
-  // sym(dest) {
-  //   this.SYM = new MakeEdge();
-  //   this.SYM.DATA = dest;
-  //   this.SYM.SYM = this;
-  //   this.LNEXT = this.SYM;
-  //   this.SYM.LNEXT = this;
-  // }
   cleave(e) {
-    //const newONEXT = e.ONEXT(0);
-    //const currONEXT = this.ONEXT(0);
     const newONEXT = e.ONEXT();
     const currONEXT = this.ONEXT();
     this.NEXT = newONEXT;
@@ -167,77 +115,7 @@ class MakeEdge {
 
     const alphaONEXT = e.ONEXT().ROT().ONEXT();
     const betaONEXT = this.ONEXT().ROT().ONEXT();
-    // this.ONEXT().r[1] = alphaONEXT;
-    // e.ONEXT().r[1] = betaONEXT;
     this.ONEXT().ROT().NEXT = alphaONEXT;
     e.ONEXT().ROT().NEXT = betaONEXT;
-
-    //this.ONEXT(0) = newONEXT;
-    //e.ONEXT = currONEXT;
-
-    // const newONEXT = e.ONEXT;
-    // const currONEXT = this.ONEXT;
-    // this.ONEXT = newONEXT;
-    // e.ONEXT = currONEXT;
-  }
-
-
-  // sym() {
-  //   const sym = new MakeEdge();
-  //   sym.data =  dest;
-  // }
-  // ccw(v1, v2, v3) {
-  //   let ccw = false;
-  //   let a = p5.Vector.sub(v2,v1);
-  //   let b = p5.Vector.sub(v3,v2);
-  //   const det = a.x * b.y - a.y * b.x;
-  //   if (det < 0) {
-  //     ccw = true;
-  //   }
-  //   return ccw;
-  // }
-  // cleave(lnext) {
-  //   let dir = this.dest.x == lnext.org.x && this.dest.y == lnext.org.x;
-  //   if (!this.LNEXT) {
-  //     this.LNEXT = lnext;
-  //     lnext.ONEXT = this;
-  //   } else {
-  //     console.log(this.ccw(this.org, this.dest, lnext.dest));
-  //     //console.log(lnext.dest);
-  //     if (this.ccw(this.org, this.dest, lnext.dest)) {
-  //       const temp = this.LNEXT;
-  //       this.DNEXT = temp;
-  //       this.LNEXT = lnext;
-  //       lnext.ONEXT = this;
-  //       lnext.RNEXT = this.DNEXT;
-  //       lnext.cleave(this.DNEXT);
-  //     } else if (!this.DNEXT){
-  //       this.DNEXT = lnext;
-  //       lnext.LNEXT = this;
-  //     } else if (!this.ccw(this.DNEXT.org, this.DNEXT.dest, lnext.dest)) {
-  //       this.DNEXT = lnext;
-  //       lnext.LNEXT = this;
-  //     }
-  //     console.log("But boss... we already got one");
-  //   }
-  //}
-  show() {
-    // let curr = this;
-    //   do {
-    //     strokeWeight(1);
-    //     line(curr.LNEXT.org.x,curr.LNEXT.org.y,curr.LNEXT.dest.x,curr.LNEXT.dest.y);
-    //     curr = curr.LNEXT;
-    //   } while (curr.LNEXT && curr.LNEXT !== this);
-    // strokeWeight(1);
-    // line(this.org.x, this.org.y, this.dest.x, this.dest.y);
-    strokeWeight(1);
-    if (this.LNEXT) {
-      stroke(255,0,0);
-      line(this.dest.x, this.dest.y, this.LNEXT.dest.x, this.LNEXT.dest.y);
-    }
-    if (this.DNEXT) {
-      stroke(0,255,0);
-      line(this.dest.x,this.dest.y,this.DNEXT.dest.x, this.DNEXT.dest.y);
-    }
   }
 }
